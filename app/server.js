9c4f4bbb9dd0aec5533ac11b8e69bb771e948606
Nerman N <nerman.nicholas@gmail.com>
2018-03-01 14:17:27 -0500

    heroku server fixes

--- app/server.js
+++ app/server.js
@@ -8,7 +8,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 var http = express();
-http.use(express.bodyParser());
 
 // Get Together Server API
 
