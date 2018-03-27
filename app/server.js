9f7c49708f5f07d2456a022f3fbd27425259c24f
Michael Kang <michael_kang@outlook.com>
2018-03-27 04:17:57 -0400

    small edits

--- app/server.js
+++ app/server.js
@@ -124,34 +124,7 @@ http.post('/register', (request, response) => {
 
     users.createUser(first_name, last_name, id, email, pic, auth);
     console.log(request.body);
-    console.log(request.body.picture.data.url);
-    // var result = {
-    //     userid: 1,
-    //     phone_number: 9053990223,
-    //     first_name: 'Michael',
-    //     last_name: 'Kang',
-    //     profile_url:'https://scontent.fyzd1-1.fna.fbcdn.net/v/t31.0-8/22426246_10155760490002964_7691357498054086805_o.jpg?oh=c3586afce9dfff9c8ce53e443b2c57b5&oe=5B0F9969',
-    //     email:'michael_kang@outlook.com',
-    //     auth_token:'',
-    //     created: Date.now(),
-    //     last_login: Date.now()
-    // };
-
-    // if(request.body != null) {
-    //     if(request.body.first_name === "Michael" &&
-    //     request.body.last_name === "Kang" &&
-    //     request.body.profile_url !== "" &&
-    //     request.body.email === "michael_kang@outlook.com") {
-    //         result.profile_url = request.body.profile_url;
-    //         result.auth_token = reqeust.body.auth_token;
-    //         var wrapper = {user: result, message: "Registration successful"};
-    //         response.send(200, JSON.stringify(wrapper));
-    //     }
-    //     else {
-    //         response.send(400, "Incorrect info when registering.");
-    //     }
-    // }
-    // response.send(500, 'Server error retrieving request.');
+    
 });
 
 http.post('/login', (request, response) => {
