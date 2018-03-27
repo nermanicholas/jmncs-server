30392fc04e96c7f627abafe21bf75972ac0b9e93
Michael Kang <michael_kang@outlook.com>
2018-03-27 03:16:08 -0400

    users test

--- app/models/users.js
+++ app/models/users.js
@@ -11,7 +11,14 @@ function createUser(first_name, last_name, fb_userid, email, profile_img, fb_aut
         var cn = 'postgres://jsxsnawiawgkxp:d942d97663ae984e9048fdcab5df5afd303e74fd20ac3ce7f8782fcccc271e29@ec2-54-225-249-161.compute-1.amazonaws.com:5432/df5u1ks7mi2i34';
         var client = new pg.Client(cn);
         client.connect();
-        var query = app.db_queries.insert_user;
+        var query = "INSERT INTO public.users(first_name, last_name, fb_userid, email, profile_img, fb_auth_token) \
+                        VALUES($1, $2, $3, $4, $5, $6) ON CONFLICT (fb_userid) \
+                        DO UPDATE SET first_name = excluded.first_name, \
+                        last_name = excluded.last_name, \
+                        email = excluded.email, \
+                        profile_img = excluded.profile_img, \
+                        fb_auth_token = excluded.fb_auth_token \
+                        RETURNING *";
         var params = [first_name, last_name, fb_userid, email, profile_img, fb_auth_token];
         client.query(query, params, (err, res) => {
         if (err) {
