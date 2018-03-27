1df840ac735e4f02df793d1c5610945e98b4942a
Michael Kang <michael_kang@outlook.com>
2018-03-27 00:54:37 -0400

    fixed nerman's potatoness

--- app/models/attractions.js
+++ app/models/attractions.js
@@ -1,63 +1,40 @@
-var app = require("../../app.json")
-var db = require("pg");
-
-
-
-function connect(_action) {
-    var result = null;
-    var pool = new db.Pool(app['postgresql']);
-    pool.connect((error, client, done) => {
-        if(error) {
-            console.log("Error: Could not establish connection with database.");
-            console.log(error);
-            return null;
-        }
-        result = _action(client);
-        done();
-    })
-    pool.end();
-    return result;
-}
+var bodyParser = require("body-parser");
+var app = require("../../app.json");
+var pg = require('pg');
+pg.defaults.ssl=true;
 
 function createAttraction(name, geolocation, rating, city, type, reviews) {
     if((name === null || name === "") || rating === null || typeof(rating) !== 'object' || (city === null || city === "") || (type === null || type === "")) {
         console.log("Error: Could not create attraction due to missing or incorrect data.");
         return null;
     } else {
-        connect((client) => {
-            client.query(app["db_queries"]["insert_attraction"], 
-                name, geolocation.toString(), JSON.stringify(rating), city, type, JSON.stringify(reviews),
-                (error, result) => {
-                    if(error) {
-                        console.log(error);
-                        return null;
-                    }
-                }).on("row", (row, result) => {
-                    return row;
-            });
-        })
+        var response = null;
+        var cn = 'postgres://jsxsnawiawgkxp:d942d97663ae984e9048fdcab5df5afd303e74fd20ac3ce7f8782fcccc271e29@ec2-54-225-249-161.compute-1.amazonaws.com:5432/df5u1ks7mi2i34';
+        var client = new pg.Client(cn);
+        client.connect();
+       
+        //return response;
+        /*
+        const client = new db.Client({
+            connectionString: app.connection_string,
+            ssl: true
+        });
+        client.ssl = true;
+        client.connect();
+        client.ssl = true;
+        const query = client.query(app.db_queries.insert_attraction, [name, geolocation.toString(), rating.toString(), city, type, reviews.toString()]);
+        query.on('end', (error, result) => {
+            response = result;
+            client.end();
+        }).then(() => {
+            return response;
+        });*/
     }
 }
 
 
 function updateAttraction(name, geolocation, rating, city, type, reviews) {
-    if((name === null || name === "") || rating === null || typeof(rating) !== 'object' || (city === null || city === "") || (type === null || type === "")) {
-        console.log("Error: Could not update attraction due to missing or incorrect data.");
-        return null;
-    } else {
-        connect((client) => {
-            client.query(app["db_queries"]["update_attraction"], 
-                name, geolocation.toString(), JSON.stringify(rating), city, type, JSON.stringify(reviews),
-                (error, result) => {
-                    if(error) {
-                        console.log(error);
-                        return null;
-                    }
-                }).on("row", (row, result) => {
-                    return row;
-            });
-        })
-    }
+
 }
 
 function deleteAttraction(attraction_id) {
@@ -70,6 +47,6 @@ function duplicateRecords(record) {
 }
 
 
-/*var test = createAttraction("Mikey's Eatery", (100, -100), {rating: 5, source: "Yelp", total: 5}, "Waterloo", "Food", [{name: "Nerman", rating: "5.0", source: "Yelp", comment: "This is awesome food!"}]);
+var test = createAttraction("Mikey's Eatery", (100, -100), {rating: 5, source: "Yelp", total: 5}, "Waterloo", "Food", [{name: "Nerman", rating: "5.0", source: "Yelp", comment: "This is awesome food!"}]);
 console.log(test);
-console.log('done');*/
+console.log('done');
